5832435c8183b95e1af48d613f51e5270718af8a
Carlos Grillet <carlosbeta5000@gmail.com>
2025-07-18 23:22:01 +0200

    enh(main): logging added

--- main.go
+++ main.go
@@ -5,13 +5,25 @@ import (
 	"net/http"
 )
 
-func root(w http.ResponseWriter, _ *http.Request) {
+var port = "8080"
+
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("{\"method\": \"%s\", \"path\": \"%s\", \"remoteAddr\": \"%s\"}\n", r.Method, r.URL.Path, r.RemoteAddr)
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "ok\n")
 }
 
+func secure(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("{\"method\": \"%s\", \"path\": \"%s\", \"remoteAddr\": \"%s\"}\n", r.Method, r.URL.Path, r.RemoteAddr)
+	w.WriteHeader(http.StatusUnauthorized)
+	fmt.Fprintf(w, "Unauthorized\n")
+}
+
 func main() {
+	fmt.Printf("Listening on port %s\n", port)
 	http.HandleFunc("/", root)
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	http.HandleFunc("/secure", secure)
+	err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil)
 	if err != nil {
 		fmt.Printf("Error ocurrend in the server: %s", err)
 	}
